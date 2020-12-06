e3fdc45b4a07b5dc08bf87c20aa52ec73d5af173
Starinin Andrey <starininandrey@gmail.com>
2020-12-06 14:21:34 +0300

    Проект

---
+++ mainwindow.cpp
@@ -0,0 +1,47 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_button_Clear_clicked()
+{
+    ui->input_Name->clear();
+    ui->comboBox_Sex->setCurrentIndex(-1);
+    ui->date_BirthDay->setDate(QDate(1900, 1, 1));
+
+    ui->statusbar->showMessage("Очищено!");
+}
+
+void MainWindow::on_button_Save_clicked()
+{
+    auto name = ui->input_Name->text();
+    auto sex = ui->comboBox_Sex->currentText();
+    QDate birthDay = ui->date_BirthDay->date();
+
+    Person person(name, sex, birthDay);
+
+    listPerson.append(person);
+
+    ui->statusbar->showMessage("Данные успешно сохранены");
+}
+
+void MainWindow::on_button_ShowAllPersons_clicked()
+{
+    QString temp = "";
+    foreach (auto person, listPerson)
+    {
+        temp += person.GetName() + " " + person.GetSex() + " " + person.GetBirthDay().toString(Qt::SystemLocaleShortDate) + "\n";
+    }
+
+    ui->label_AllPersons->setText(temp);
+}
